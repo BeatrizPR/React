4dbcb493703964cdad2568072d3ddd5108144666
BeatrizPR <artismusas@gmail.com>
2023-04-13 15:48:00 +0200

    Añadido el total

--- part1/src/index.js
+++ part1/src/index.js
@@ -14,6 +14,12 @@ const Content=(props) =>{
   )
 }
 
+const Total=(props) =>{
+  return(
+    <p>Number of exercices {props.exercises1 + props.exercises2 + props.exercises3}</p>
+  )
+}
+
 const Footer = () => {
   return (
     <div>
@@ -37,6 +43,7 @@ const App = () => {
       <Content part={part1} exercises={exercises1} />
       <Content part={part2} exercises={exercises2} />
       <Content part={part3} exercises={exercises3} />
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
       <Footer/>
     </div>
   )
